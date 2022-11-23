f1b839f5a65badb970137fdba9e8f9cba77876b9
Y.Zhao <83700920+CandiceZhao28@users.noreply.github.com>
2022-11-23 10:16:54 +0000

    Update streamlit_app.py
    Adjust the multiselect list and the table to show on the page, so that
    1.  The list still shows all the fruit availble, but 
    2. the table only show info of fruits that are selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,15 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Add the fruit list availiable for smoothie as a Pandas DataFrame
 import pandas as pd
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-# Use column `Fruit` as index of the DataFrame
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit') # Use column `Fruit` as index of the DataFrame
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 # Also, we give an example - pre-populate the list
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+# Create a DataFrame only showing rows of fruits that are selected
+fruits_to_show = my_fruit_list.loc['fruit_selected']
 
-# Display the table on the page
-streamlit.dataframe(my_fruit_list)
+# Display the table of selected fruits on the page
+streamlit.dataframe(fruits_to_show)
 
 
